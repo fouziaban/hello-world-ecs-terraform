1dfecb830ee71375339c86a74cd4e60e5e8c5a96
SHAIK FOUZIA BANU <136546551+fouziaban@users.noreply.github.com>
2024-06-08 14:38:04 +0300

    Update remote_backend.tf

--- infra/accounts/dev/ecs/remote_backend.tf
+++ infra/accounts/dev/ecs/remote_backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     key            = "dev/ecs/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "hello-world-state-locks"
+    dynamodb_table = "hello-world-state-locks-db"
     encrypt        = true
     profile        = "asmigar"
   }
