99486d2541a4cfb1f7ab3f0153b35f867b52dd3b
SHAIK FOUZIA BANU <136546551+fouziaban@users.noreply.github.com>
2024-06-08 16:17:53 +0300

    Update main.tf

--- infra/accounts/dev/remote_state/main.tf
+++ infra/accounts/dev/remote_state/main.tf
@@ -32,7 +32,7 @@ resource "aws_s3_bucket_versioning" "terraform_state" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "hello-world-state-locks-db1"
+  name         = "hello-world-state-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
